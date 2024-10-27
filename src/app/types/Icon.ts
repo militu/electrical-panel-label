ce35e2d406d573f60db875859cea73fa3c81ccb6
militu <mazzeo.victor@gmail.com>
2024-10-27 21:45:29 +0000

    Add new icons

--- src/app/types/Icon.ts
+++ src/app/types/Icon.ts
@@ -1,52 +1,56 @@
 export type IconName =
-    | "air-conditioner"
-    | "boiler"
-    | "car-charger"
-    | "circuit-breaker"
-    | "differential-switch"
-    | "dishwasher"
-    | "doorbell"
-    | "dryer"
-    | "freezer"
-    | "fridge"
-    | "hood"
-    | "impulse-relay"
-    | "intercom"
-    | "lighting"
-    | "microwave"
-    | "off-peak-contactor"
-    | "oven"
-    | "radiator"
-    | "shutters"
-    | "socket-plug"
-    | "stove"
-    | "ventilation"
-    | "washing-machine"
-    | "water-heater";
+  | "air-conditioner"
+  | "boiler"
+  | "car-charger"
+  | "circuit-breaker"
+  | "differential-switch"
+  | "dishwasher"
+  | "doorbell"
+  | "dryer"
+  | "freezer"
+  | "fridge"
+  | "hood"
+  | "impulse-relay"
+  | "intercom"
+  | "lighting"
+  | "lightning-protection"
+  | "microwave"
+  | "modular-switch"
+  | "off-peak-contactor"
+  | "oven"
+  | "radiator"
+  | "shutters"
+  | "socket-plug"
+  | "stove"
+  | "ventilation"
+  | "washing-machine"
+  | "water-heater";
 
 export const iconList: IconName[] = [
-    "air-conditioner",
-    "boiler",
-    "car-charger",
-    "circuit-breaker",
-    "differential-switch",
-    "dishwasher",
-    "doorbell",
-    "dryer",
-    "freezer",
-    "fridge",
-    "hood",
-    "impulse-relay",
-    "intercom",
-    "lighting",
-    "microwave",
-    "off-peak-contactor",
-    "oven",
-    "radiator",
-    "shutters",
-    "socket-plug",
-    "stove",
-    "ventilation",
-    "washing-machine",
-    "water-heater"
+  "air-conditioner",
+  "boiler",
+  "car-charger",
+  "circuit-breaker",
+  "differential-switch",
+  "dishwasher",
+  "doorbell",
+  "dryer",
+  "freezer",
+  "fridge",
+  "hood",
+  "impulse-relay",
+  "intercom",
+  "lighting",
+  "lightning-protection",
+  "microwave",
+  "modular-switch",
+  "off-peak-contactor",
+  "oven",
+  "radiator",
+  "shutters",
+  "socket-plug",
+  "stove",
+  "ventilation",
+  "washing-machine",
+  "water-heater",
 ];
