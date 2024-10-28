cd3fd26a84351a48591bfca3f2dfb3b74a4c6cca
militu <mazzeo.victor@gmail.com>
2024-10-28 14:48:59 +0000

    Add ability to select half sizes

--- src/app/types/Unit.ts
+++ src/app/types/Unit.ts
@@ -1,11 +1,12 @@
 export type Unit = {
-    id: string;
-    size: number;
-    description: string;
-    description_color: string;
-    description_font_size: number;
-    logo: string | null;
-    sidebar_color: string;
-    sidebar_width: number;
-    bottom_color: string;
+  id: string;
+  allow_half_sizes: boolean;
+  size: number;
+  description: string;
+  description_color: string;
+  description_font_size: number;
+  logo: string | null;
+  sidebar_color: string;
+  sidebar_width: number;
+  bottom_color: string;
 };
